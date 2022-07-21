814abe9921c38061c4647f14701576cca8327839
AmandaAzania <mandygudlwa@gmail.com>
2022-07-21 10:30:35 +0200

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-footer dark padless>
+    <v-card
+      flat
+      tile
+      class="black white--text py-5 px-5 text-center"
+      width="100%"
+    >
+      <v-row>
+        <v-col cols="12" sm="4">
+          <v-card-text class="white--text pt-2">
+            <v-btn icon>
+              <v-icon color="#A7121D"> mdi-bee </v-icon>
+            </v-btn>
+          </v-card-text>
+        </v-col>
+
+        <v-col cols="12" sm="4">
+          <v-card-text class="pt-2">
+            <v-btn
+              v-for="icon in icons"
+              :key="icon"
+              class="mx-1 white--text"
+              icon
+            >
+              <v-icon size="24px">
+                {{ icon }}
+              </v-icon>
+            </v-btn>
+          </v-card-text>
+        </v-col>
+        <v-col cols="12" sm="4">
+          <v-card-text class="pt-2">
+            <v-btn class="mx-1 white--text" icon>
+              <v-icon color="#A7121D">mdi-heart</v-icon>
+            </v-btn>
+            <div class="footer-copyright text-center py-3">© 2022 Copyright   <span style="color: rgba(172, 8, 8, 0.884)">  Amanda Gudlwa</span>
+                </div>
+          </v-card-text>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar app color="#111111" dark flat class="px-8">
+    <v-btn icon>
+      <v-icon color="#A7121D">mdi-bee</v-icon>
+    </v-btn>
+
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll('home')"> <v-icon>mdi-home-outline</v-icon> Home</v-btn>
+       <v-btn text @click="scroll('home')"><v-icon>mdi-account-circle</v-icon> About</v-btn>
+       <v-btn text @click="scroll('skills')"><v-icon>mdi-cogs</v-icon> skills</v-btn>
+    <v-btn text @click="scroll('projects')"> <v-icon>mdi-folder-outline</v-icon> Projects</v-btn>
+     <v-btn text @click="scroll('project')"> <v-icon>mdi-book</v-icon> CV</v-btn>
+  <v-btn text @click="scroll('contact')"><v-icon>mdi-phone-outline</v-icon> Contact</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({ behavior: "smooth" });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
